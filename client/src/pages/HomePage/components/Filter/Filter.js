298b9deaf6565750be3e7ddc4f7c92d568bd2936
unknown <aalshanbayev@gmail.com>
2020-06-16 15:44:42 -0400

    few changes in filter

--- client/src/pages/HomePage/components/Filter/Filter.js
+++ client/src/pages/HomePage/components/Filter/Filter.js
@@ -6,6 +6,7 @@ import shoesManufacturer from './datashoesManufacturer';
 import shoesModels from './datashoesModels';
 
 const animatedComponents = makeAnimated();
+const shoesManufacturerNumber = shoesManufacturer;
 
 
 class Filter extends React.Component {
@@ -13,40 +14,37 @@ class Filter extends React.Component {
     shoesModelsFiltered: []
   }
 
-  handleChosenProducers = (selectedProducers) => {
-    console.log('Outcome from the first Select: ', selectedProducers);
-    console.log(selectedProducers.length) //If delete all brands error pops-up cause the property's value is null (not '0')
-    console.log('shoesModels', shoesModels);
-    console.log('selectedProducers[0].label:  ', selectedProducers[0].label);
-    
+  handleChosenProducers = (selectedArray) => {    
+    let len = 0;
+    let selectedProducers = [];
     let shoesModelsFiltered = [];
-
-    for (let i=0; i < selectedProducers.length; i++) {
+    
+    if (selectedArray === null) {
+      len = shoesManufacturer.length;
+      selectedProducers = shoesManufacturer;
+    } else {
+        len = selectedArray.length;
+        selectedProducers = selectedArray;
+      };
+    
+    for (let i=0; i < len; i++) {
       shoesModelsFiltered = shoesModelsFiltered.concat(shoesModels.filter(models => models.company === selectedProducers[i].label.toLowerCase())) 
     }
-    
-    console.log('shoesModelsFiltered', shoesModelsFiltered);
     
     this.setState({
       shoesModelsFiltered: shoesModelsFiltered
     })
   }
 
-/*   handleChosenModels = (selectedModels) => {
-    console.log('Outcome from the first Select: ', selectedModels);
-    this.setState({
-      models: selectedModels
-    })
-  } */
-
   render() {
     return (
       <div className="filter">
         <section className="filter__by-manufacturer">
           <Select 
+            placeholder='Select Brand'
             closeMenuOnSelect={false}
             components={animatedComponents}
-            defaultValue={shoesManufacturer[0]}
+            defaultValue={0}
             isMulti
             onChange = {this.handleChosenProducers}
             options={shoesManufacturer}
@@ -55,13 +53,28 @@ class Filter extends React.Component {
         </section>
         <section className="filter__by-model">
           <Select 
+            placeholder='Select Models'
             closeMenuOnSelect={false}
             components={animatedComponents}
-            defaultValue={shoesModels[0], shoesModels[1]}
+            defaultValue={0}
             isMulti
             options={this.state.shoesModelsFiltered}
             onChange={this.handleChosenModels}
           />
+        </section>
+        <section>
+          <div>
+            <input type="checkbox" id="black" name="Black" checked />
+            <label for="black">Black</label>
+          </div>
+          <div>
+            <input type="checkbox" id="white" name="White" checked />
+            <label for="white">White</label>
+          </div>
+          <div>
+            <input type="checkbox" id="red" name="red" checked />
+            <label for="red">Red</label>
+          </div>
         </section>
       </div>
     )
