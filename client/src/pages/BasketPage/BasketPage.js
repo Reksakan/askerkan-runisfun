f3577c643698ed435fc4883a922904e5302a0304
unknown <aalshanbayev@gmail.com>
2021-03-30 22:54:59 -0400

    heroku v01

--- client/src/pages/BasketPage/BasketPage.js
+++ client/src/pages/BasketPage/BasketPage.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './BasketPage.scss'
 import InventoryToBuy from './components/InventoryToBuy';
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_API_URL; 
 
 class BasketPage extends React.Component {
   state = {
