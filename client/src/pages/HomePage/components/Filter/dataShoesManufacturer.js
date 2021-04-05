478a61460258a6d28b83d80090ae727f08ebf9ad
Alex Ni <12097569+nialexsan@users.noreply.github.com>
2021-04-05 00:40:21 -0400

    client build

---
+++ client/src/pages/HomePage/components/Filter/dataShoesManufacturer.js
@@ -0,0 +1,7 @@
+const shoesManufacturer = [
+  {value: 'adidas', label: 'adidas'},
+  {value: 'nike', label: 'nike'},
+  {value: 'salomon', label: 'salomon'},
+];
+
+export default shoesManufacturer; 
